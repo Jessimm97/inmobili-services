321c10b9ac6d57519a4dd4c6847e95a3c6ca7c70
CARLOS FERNANDO LEAL OLIVA <paintsoliva@gmail.com>
2025-12-26 15:36:37 -0500

    micro service created

---
+++ micro-eureka/src/test/java/com/micro/eureka/micro_eureka/MicroEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.eureka.micro_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
