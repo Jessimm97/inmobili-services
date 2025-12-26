aecd0dadb01071156882793b820ecd3ff4a76702
CARLOS FERNANDO LEAL OLIVA <paintsoliva@gmail.com>
2025-12-26 16:12:21 -0500

    micro gateway added

---
+++ micro-gateway/src/test/java/com/micro/gateway/micro_gateway/MicroGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.gateway.micro_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
